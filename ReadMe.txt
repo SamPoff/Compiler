c0a95a7e6fb59737af189ee903cd566030ec2f32
sam <sam@sam-ubuntu>
2018-08-29 15:46:16 -0700

    Added ReadMe

---
+++ ReadMe.txt
@@ -0,0 +1,26 @@
+Navigate to 'src' folder in terminal and enter the following commands successively. 
+
+javac lexer/*.java
+javac symbols/*.java
+javac inter/*.java
+javac parser/*.java
+javac main/*.java
+java main.Main 
+ 
+<paste in the following>
+
+{
+int i; int j; float v; float x; float[100] a;
+
+while( true ){
+
+    do i = i + 1; while ( a[i] < v);
+    do j = j - 1; while ( a[j] > v);
+    if ( i >= j) break;
+    x  = a[i]; a[i] = a[j]; a[j] = x;
+
+    }
+
+}
+
+<type in a random letter and hit enter>
--- src/main/Main.java
+++ src/main/Main.java
@@ -10,18 +10,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 //        if(args.length >= 2){
-    	
-        try{
-            FileInputStream fis=new FileInputStream("/home/sam/eclipse-workspace/Compiler/src/in0.c");
-            System.setIn(fis);
-        	System.out.println("here");
-        }
-        catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
-
-        PrintStream ps=new PrintStream(new FileOutputStream(args[1]));
-        System.setOut(ps);
+//    	
+//        try{
+//            FileInputStream fis=new FileInputStream("/home/sam/eclipse-workspace/Compiler/src/in0.c");
+//            System.setIn(fis);
+//        	System.out.println("here");
+//        }
+//        catch (FileNotFoundException e){
+//            e.printStackTrace();
+//        }
+//
+//        PrintStream ps=new PrintStream(new FileOutputStream(args[1]));
+//        System.setOut(ps);
 //        }
 
         Lexer lex = new Lexer();
